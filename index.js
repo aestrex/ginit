a50cd9c0cb2eec0a93cf3c58d6d21f56aed072fd
David Alamu <aestrex@gmail.com>
2019-01-31 22:20:00 +0100

    add questions to inquirer module

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const clear = require('clear');
 const figlet = require('figlet');
 
 const files = require('./lib/files');
+const inquirer = require('./lib/inquirer');
 
 clear();
 console.log(
@@ -13,7 +14,14 @@ console.log(
   )
 );
 
-if (files.directoryExists('.git')) {
-  console.log(chalk.red('Already a git repository!'));
-  process.exit();
+// if (files.directoryExists('.git')) {
+//   console.log(chalk.red('Already a git repository!'));
+//   process.exit();
+// }
+
+const run = async () => {
+  const credentials = await inquirer.askGithubCredentials();
+  console.log(credentials);
 }
+
+run();
