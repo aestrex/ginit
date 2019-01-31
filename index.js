d70b6f6789311156f1e6efc314fec33aae595aa8
David Alamu <aestrex@gmail.com>
2019-01-31 22:09:18 +0100

    display starting text

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const clear = require('clear');
 const figlet = require('figlet');
 
 const files = require('./lib/files');
+
+clear();
+console.log(
+  chalk.yellow(
+    figlet.textSync('codegod', {
+      horizontalLayout: 'full'
+    })
+  )
+);
